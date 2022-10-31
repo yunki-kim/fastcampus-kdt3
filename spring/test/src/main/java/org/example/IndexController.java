828a2e7ee5699c91053ac866182f2e69a2d7c20b
Yuki <skool109@gmail.com>
2022-10-31 16:38:00 +0900

    commit

---
+++ spring/test/src/main/java/org/example/IndexController.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Controller
+public class IndexController {
+
+    private IndexController() {}
+
+    // HTTP GET localhost:8080/
+    // @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
+    public String index(Locale locale, Model model, HttpServletRequest request) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        model.addAttribute("now", localDateTime);
+        // model: HttpServletRequest
+        // request.setAttribute("now", localDateTime);
+        // localhost:8080 -- forward -> /WEB-INF/views/index.jsp
+
+        System.out.println("IndexController");
+
+        // /WEB-INF/views/index.jsp
+        return "index";
+    }
+}
